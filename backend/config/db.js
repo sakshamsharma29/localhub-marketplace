3f60a67dad1331cbccc99225f6de8c9ddbd36fe8
sakshamsharma29 <saksham.sharma292006@gmail.com>
2026-03-23 21:29:58 +0530

    use railway mysql variable

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'localhub',
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
